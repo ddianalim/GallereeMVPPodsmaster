a5ca543d9721ab35e0d21e83a7809e98906a6c38
dianalimnos <dianalim1117@gmail.com>
2016-07-22 15:38:44 -0700

    added in parse connection code

--- Galleree/AppDelegate.swift
+++ Galleree/AppDelegate.swift
@@ -7,27 +7,26 @@
 //
 
 import UIKit
+import Parse
 import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Set up the Parse SDK
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "galleree"
+            $0.server = "https://galleree.herokuapp.com/parse"
+        }
+        Parse.initializeWithConfiguration(configuration)
+        
         return true
     }
     
-/*func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
- // Override point for customization after application launch.
- UITabBar.appearance().translucent = false
- UITabBar.appearance().barTintColor = UIColor(rgba: "#640E27")
- UITabBar.appearance().tintColor = UIColor.whiteColor()
- return true
- }*/
-
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
